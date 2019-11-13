63c4cdd2c5a5f19a20711fb44a74cf0aa9c7de73
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 18:20:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,6 +15,13 @@ class Waiter
     Meal.select.each{|meal| meal.waiter == self}
   end
   
+  def customers
+    while
+    customer = meals[i].customer
+    meals.max do |meal| 
+      (meal_a.tip/meal_a.total) <=> (meal_b.tip/meal_b.total) 
+    end
+  
   def all 
     @@all 
   end 
