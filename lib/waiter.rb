948c50f461700f2caf766a663857be44d586e074
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 17:56:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience 
+  @@all = []
+  def initialize(name, experience)
+    @name = name 
+    @yrs_experience = experience 
+    @@all << self 
+  end 
+  
+  def all 
+    @@all 
+  end 
 end
